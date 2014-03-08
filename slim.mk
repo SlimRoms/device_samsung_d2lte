e3a0151548ee4def7bcdf6259459f99d6c3a1736
nicklovell23 <nicklovell23@gmail.com>
2014-03-09 00:14:43 +0100

    Slimkat Unified

---
+++ slim.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, device/samsung/d2lte/full_d2lte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+PRODUCT_NAME := slim_d2lte
